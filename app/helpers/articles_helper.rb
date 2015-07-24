ccd99cb3ab9e5d01d78c495c3771b686fc69d02f
varun324 <varunwadhwani324@mail.com>
2015-07-24 16:38:01 +0530

    blogger commit

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,11 @@
+module ArticlesHelper
+
+ def article_params
+    params.require(:article).permit(:title, :body, :tag_list, :image)
+  end
+
+
+end
+
+
+
---
+++ app/models/tag.rb
@@ -0,0 +1,12 @@
+class Tag < ActiveRecord::Base
+
+	has_many :taggings
+	has_many :articles, through: :taggings
+
+
+  def to_s
+    name
+  end
+
+
+end
